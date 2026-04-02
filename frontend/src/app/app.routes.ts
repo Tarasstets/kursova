e2b32d72fbc615cf0a154b181d775976c06b78ab
Tarasstets <152523184+Tarasstets@users.noreply.github.com>
2026-04-02 21:57:55 +0300

    add settings

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,12 +6,18 @@ import { AdminComponent } from './pages/admin/admin';
 import { authGuard } from './guards/auth.guard';
 import { adminGuard } from './guards/admin.guard';
 import { RegisterComponent } from './pages/register/register';
+import { SettingsComponent } from './pages/settings/settings';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+
+  { path: 'home', component: TasksComponent, canActivate: [authGuard] },
   { path: 'tasks', component: TasksComponent, canActivate: [authGuard] },
   { path: 'analytics', component: AnalyticsComponent, canActivate: [authGuard] },
-  { path: 'register', component: RegisterComponent },
-  { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] }
+  { path: 'settings', component: SettingsComponent, canActivate: [authGuard] },
+  { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
+
+  { path: '**', redirectTo: 'login' }
 ];
